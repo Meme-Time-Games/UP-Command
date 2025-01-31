6240cc07fee2d9b6824d5cae0c15b00123178e88
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-01-31 14:40:09 -0300

    [A] Added command frame executor

---
+++ Runtime/Implementations/FrameExecutor/Domain/CommandUpdateFrameExecutor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Commands.Core
+{
+    public class CommandUpdateFrameExecutor : MonoBehaviour
+    {
+        private ICommand _commandExecute;
+
+        public void Install(ICommand command)
+        {
+            _commandExecute = command;
+        }
+
+        private void Update()
+        {
+            _commandExecute.Execute();
+        }
+    }
+}
---
+++ Runtime/Implementations/FrameExecutor/Installers/CommandUpdateFrameExecutorInstaller.cs
@@ -0,0 +1,20 @@
+﻿using DependencyInjector.Core;
+using DependencyInjector.Installers;
+using UnityEngine;
+
+namespace Commands.Core
+{
+    public class CommandUpdateFrameExecutorInstaller : SingleMonoInstaller<CommandUpdateFrameExecutor>
+    {
+        [Inject] private ICommand _commandExecute;
+
+        protected override CommandUpdateFrameExecutor GetData()
+        {
+            CommandUpdateFrameExecutor commandUpdateFrameExecutor = new GameObject("CommandUpdateFrameExecutor").AddComponent<CommandUpdateFrameExecutor>();
+            commandUpdateFrameExecutor.transform.SetParent(transform);
+            commandUpdateFrameExecutor.Install(_commandExecute);
+
+            return commandUpdateFrameExecutor;
+        }
+    }
+}
